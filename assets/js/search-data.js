44d6881e53a72081de1b9aecd2a42753f59da134
labrinid <labrinid@users.noreply.github.com>
2026-05-07 17:23:01 +0000

    Deploying to gh-pages from @ labrinid/labrinidis.com@302204538f4d5adc4dd73eb80f70f3505209bc98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,6 +65,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
+        },{id: "nav-service",
+          title: "service",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/service/";
+          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
