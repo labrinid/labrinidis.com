233d5d526a87299abd55f1b90510dc5c20efb03a
labrinid <labrinid@users.noreply.github.com>
2026-05-11 15:10:19 +0000

    Deploying to gh-pages from @ labrinid/labrinidis.com@d37a017b02717a9eb9e36a59946056a1d6f00899 🚀

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -1,6 +1,1 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-BRQ1BN1TXF");
+function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-BRQ1BN1TXF");
